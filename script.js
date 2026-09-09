726735e307f423611f2e551ca44a2f07cd84b1ff
Yshmael Jephthah Bigtas <yshmaeljephthah.bigtas@antipolo.benilde.edu.ph>
2026-09-09 16:43:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// =========================================
+// BANANA WEBSITE JAVASCRIPT
+// Add extra website functions here later.
+// =========================================
+
+console.log("Banana website loaded!");
